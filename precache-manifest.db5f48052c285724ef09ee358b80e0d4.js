abc9cc61e6138cd74af36b1fa85b30438a173ef8
Wang Zijun <wzj97@hotmail.com>
2020-03-13 16:10:25 +0800

    Updates

---
+++ precache-manifest.db5f48052c285724ef09ee358b80e0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c34c278bc07a031d00f687296c1dcc9",
+    "url": "/react-my-todo/index.html"
+  },
+  {
+    "revision": "3aef2be50cde79687e36",
+    "url": "/react-my-todo/static/css/main.ac12f51e.chunk.css"
+  },
+  {
+    "revision": "744fc2c3c383736b3a3a",
+    "url": "/react-my-todo/static/js/2.2edebecd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-my-todo/static/js/2.2edebecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aef2be50cde79687e36",
+    "url": "/react-my-todo/static/js/main.c577ddef.chunk.js"
+  },
+  {
+    "revision": "1d0e8470a0b9aa0e10fe",
+    "url": "/react-my-todo/static/js/runtime-main.df523cab.js"
+  }
+]);
